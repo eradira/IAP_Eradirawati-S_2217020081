b520d1a983b2b7dd3cb4d930f265269d5fccd739
eradira <eradirawati25@gmail.com>
2025-06-01 12:02:42 +0700

    Upload wpu-rest-client vidio 9

---
+++ wpu-rest-client/application/views/home/index.php
@@ -0,0 +1,112 @@
+
+
+    <div class="jumbotron" id="home">
+      <div class="container">
+        <div class="text-center">
+          <img src="<?= base_url(); ?>assets/img/profil dira.jpeg" class="rounded-circle img-thumbnail">
+          <h1 class="display-4">Eradirawati S</h1>
+          <h3 class="lead"> Student | Programmer | Youtuber</h3>
+        </div>
+      </div>
+    </div>
+
+    <!-- Portfolio -->
+    <section class="portfolio" id="portfolio">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>About Me</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/dira.jpeg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Eradirawati S</p>
+                <p class="card-text">Lahir di Jambi 25 mei 2004. Saat ini sedang menempuh pendidikan sebagai mahasiswa semester 6 di jurusan sistem informasi UIN Imam Bonjol Padang.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/yt.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Saya aktif di YouTube dengan membagikan vlog keseharian yang sederhana dan apa adanya. Lewat konten tersebut, saya ingin berbagi cerita, pengalaman, dan sedikit hiburan dari sudut pandang pribadi.
+Channel ini adalah ruang untuk mengekspresikan diri tanpa harus menjadi sempurna.
+
+</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md mb-4">
+            <div class="card">
+              <img class="card-img-top" src="<?= base_url(); ?>assets/img/thumbs/design.jpg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Saya juga suka mendesain, mulai dari mengedit foto, video, hingga mempelajari desain antarmuka (UI) dan hal-hal seputar visual digital. Kegiatan ini jadi cara saya untuk menyalurkan kreativitas dan terus belajar hal-hal baru di dunia desain.</p>
+              </div>
+            </div>
+          </div>   
+        </div>
+
+     
+     
+    </section>
+
+
+    <!-- Contact -->
+    <section class="contact bg-light" id="contact">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Contact</h2>
+          </div>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-lg-4">
+            <div class="card bg-primary text-white mb-4 text-center">
+              <div class="card-body">
+                <h5 class="card-title">Contact Me</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+            
+            <ul class="list-group mb-4">
+              <li class="list-group-item"><h3>Location</h3></li>
+              <li class="list-group-item">My Home</li>
+              <li class="list-group-item">Jl. Rajawali no 10, Jambi</li>
+              <li class="list-group-item">Jambi, Indonesia</li>
+            </ul>
+          </div>
+
+          <div class="col-lg-6">
+            
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email">
+              </div>
+              <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" class="form-control" id="phone">
+              </div>
+              <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" rows="3"></textarea>
+              </div>
+              <div class="form-group">
+                <button type="button" class="btn btn-primary">Send Message</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </section>
